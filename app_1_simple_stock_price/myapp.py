3aeed907787d601c52bca27771f8bb5defd01357
momijizen <momijizen@hotmail.com>
2021-08-08 12:23:52 +0700

    edit

--- app_1_simple_stock_price/myapp.py
+++ app_1_simple_stock_price/myapp.py
@@ -4,7 +4,7 @@ import streamlit as st
 st.write("""
 # Simple Stock Price App
 
-Shown are the stock closing price and volume of Google!
+Shown are the stock **closing** price and **volume** of Google!
 
 """)
 
@@ -14,7 +14,7 @@ tickerSymbol = 'GOOGL'
 #get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 #get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-7-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
 st.line_chart(tickerDf.Close)
